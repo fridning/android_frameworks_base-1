815db3bd857c7f50e040a25c503fc7a9173d59b8
maxwen <max.weninger@gmail.com>
2017-09-22 18:45:17 +0200

    base: tuner: add a hide config to left and right shortcut
    change the current none to be a hide shortcut
    and add a new default to use instead
    
    Change-Id: I6fdfa68a4b083df0ae6583000862dc7bb07a75c7
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
@@ -351,8 +351,10 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
     private void updateRightAffordanceIcon() {
         IconState state = mRightButton.getIcon();
         mRightAffordanceView.setVisibility(!mDozing && state.isVisible ? View.VISIBLE : View.GONE);
-        mRightAffordanceView.setImageDrawable(state.drawable, state.tint);
-        mRightAffordanceView.setContentDescription(state.contentDescription);
+        if (state.isVisible) {
+            mRightAffordanceView.setImageDrawable(state.drawable, state.tint);
+            mRightAffordanceView.setContentDescription(state.contentDescription);
+        }
     }
 
     public void setStatusBar(StatusBar statusBar) {
@@ -378,9 +380,12 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
      * Resolves the intent to launch the camera application.
      */
     public ResolveInfo resolveCameraIntent() {
-        return mContext.getPackageManager().resolveActivityAsUser(getCameraIntent(),
-                PackageManager.MATCH_DEFAULT_ONLY,
-                KeyguardUpdateMonitor.getCurrentUser());
+        if (getCameraIntent() != null) {
+            return mContext.getPackageManager().resolveActivityAsUser(getCameraIntent(),
+                    PackageManager.MATCH_DEFAULT_ONLY,
+                    KeyguardUpdateMonitor.getCurrentUser());
+        }
+        return null;
     }
 
     private void updateCameraVisibility() {
@@ -403,8 +408,10 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
     private void updateLeftAffordanceIcon() {
         IconState state = mLeftButton.getIcon();
         mLeftAffordanceView.setVisibility(!mDozing && state.isVisible ? View.VISIBLE : View.GONE);
-        mLeftAffordanceView.setImageDrawable(state.drawable, state.tint);
-        mLeftAffordanceView.setContentDescription(state.contentDescription);
+        if (state.isVisible) {
+            mLeftAffordanceView.setImageDrawable(state.drawable, state.tint);
+            mLeftAffordanceView.setContentDescription(state.contentDescription);
+        }
     }
 
     public boolean isLeftVoiceAssist() {
@@ -458,6 +465,9 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
 
     public void bindCameraPrewarmService() {
         Intent intent = getCameraIntent();
+        if (intent == null) {
+            return;
+        }
         ActivityInfo targetInfo = PreviewInflater.getTargetActivityInfo(mContext, intent,
                 KeyguardUpdateMonitor.getCurrentUser(), true /* onlyDirectBootAware */);
         if (targetInfo != null && targetInfo.metaData != null) {
@@ -498,6 +508,9 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
 
     public void launchCamera(String source) {
         final Intent intent = getCameraIntent();
+        if (intent == null) {
+            return;
+        }
         intent.putExtra(EXTRA_CAMERA_LAUNCH_SOURCE, source);
         boolean wouldLaunchResolverActivity = PreviewInflater.wouldLaunchResolverActivity(
                 mContext, intent, KeyguardUpdateMonitor.getCurrentUser());
@@ -598,7 +611,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
         } else {
             boolean dismissShade = !TextUtils.isEmpty(mLeftButtonStr)
                     && Dependency.get(TunerService.class).getValue(LOCKSCREEN_LEFT_UNLOCK, 1) != 0;
-            mActivityStarter.startActivity(mLeftButton.getIntent(), dismissShade);
+            mActivityStarter.startActivity(PHONE_INTENT, dismissShade);
         }
     }
 
@@ -654,7 +667,9 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
             mPreviewContainer.removeView(previewBefore);
             visibleBefore = previewBefore.getVisibility() == View.VISIBLE;
         }
-        mCameraPreview = mPreviewInflater.inflatePreview(getCameraIntent());
+        if (getCameraIntent() != null) {
+            mCameraPreview = mPreviewInflater.inflatePreview(getCameraIntent());
+        }
         if (mCameraPreview != null) {
             mPreviewContainer.addView(mCameraPreview);
             mCameraPreview.setVisibility(visibleBefore ? View.VISIBLE : View.INVISIBLE);
@@ -673,7 +688,9 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
             mLeftPreview = mPreviewInflater.inflatePreviewFromService(
                     mAssistManager.getVoiceInteractorComponentName());
         } else {
-            mLeftPreview = mPreviewInflater.inflatePreview(mLeftButton.getIntent());
+            if (mLeftButton.getIntent() != null) {
+                mLeftPreview = mPreviewInflater.inflatePreview(mLeftButton.getIntent());
+            }
         }
         if (mLeftPreview != null) {
             mPreviewContainer.addView(mLeftPreview);
@@ -793,6 +810,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
                     inflateCameraPreview();
                     updateCameraVisibility();
                     updateLeftAffordance();
+                    updateRightAffordance();
                 }
             };
 
@@ -804,6 +822,10 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
     public void updateLeftAffordance() {
         updateLeftAffordanceIcon();
         updateLeftPreview();
+    }
+
+    public void updateRightAffordance() {
+        updateRightAffordanceIcon();
     }
 
     public void onKeyguardShowingChanged() {
@@ -820,7 +842,8 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
 
     private void setLeftButton(IntentButton button) {
         mLeftButton = button;
-        if (!(mLeftButton instanceof DefaultLeftButton)) {
+        IconState state = mLeftButton.getIcon();
+        if (!(mLeftButton instanceof DefaultLeftButton) || !state.isVisible) {
             mLeftIsVoiceAssist = false;
         }
         updateLeftAffordance();
