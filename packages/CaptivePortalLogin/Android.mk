b8e9b43fcb1cf29a4be43a1f72e183269bd45259
Anton Hansson <hansson@google.com>
2018-02-23 12:57:51 +0000

    frameworks/base: Set LOCAL_SDK_VERSION where possible.
    This change sets LOCAL_SDK_VERSION for all packages where
    this is possible without breaking the build, and
    LOCAL_PRIVATE_PLATFORM_APIS := true otherwise.
    
    Setting one of these two will be made required soon, and this
    is a change in preparation for that. Not setting LOCAL_SDK_VERSION
    makes the app implicitly depend on the bootclasspath, which is
    often not required. This change effectively makes depending on
    private apis opt-in rather than opt-out.
    
    Test: make relevant packages
    Bug: 73535841
    Exempt-From-Owner-Approval: Global cleanup
    Change-Id: I66b515d98d03264dbb893b7c55ba7bdddfb447a1
    Merged-In: I26458e41ecb84de91ac9a356a5d4bafb44f463c1
    Merged-In: I938efe7a2359302cbf4d5d6f786f963044e1bbbd
    Merged-In: I72ce7f1c6242ddf9a5b36075761f3f1f1138cea1
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- packages/CaptivePortalLogin/Android.mk
+++ packages/CaptivePortalLogin/Android.mk
@@ -7,6 +7,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := CaptivePortalLogin
+LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_CERTIFICATE := platform
 
 include $(BUILD_PACKAGE)
